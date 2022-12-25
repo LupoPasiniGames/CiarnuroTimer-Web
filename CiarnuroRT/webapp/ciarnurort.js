90d78cf1849b492e197995f20d3464c4b19b163c
Federico <info@fdossena.com>
2022-12-25 13:50:10 +0100

    Added HTTP redirect to HTTPS (PWAs only work via HTTPS)

--- CiarnuroRT/webapp/ciarnurort.js
+++ CiarnuroRT/webapp/ciarnurort.js
@@ -2,6 +2,9 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js');
 }
+if(location.protocol=="http:"){
+    location.href="https"+location.href.substring(4);
+}
 if (window.matchMedia('(display-mode: standalone)').matches) { //PWAs in standalone mode don't have the are you sure you want to leave the page dialog, so we prevent accidental back button presses on android from killing the app
     history.pushState({},null,document.URL);
     window.addEventListener('popstate', () => {
