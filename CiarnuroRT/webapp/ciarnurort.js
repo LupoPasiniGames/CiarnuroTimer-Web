53a683c8dc179c8bc100861cf60435806ff05df5
Davide Gaiotto <gaiottodavide1@gmail.com>
2024-06-27 10:23:33 +0200

    Added functionality that highlights players after their creation

--- CiarnuroRT/webapp/ciarnurort.js
+++ CiarnuroRT/webapp/ciarnurort.js
@@ -596,11 +596,15 @@ function gameOptionsToPlayerManagement(){
     }
 }
 function populatePlayersAndTeamsList(){
+    let i = 0;
     let list=I("players");
     list.innerHTML="";
     Array.from(players).forEach(p => {
         let d=document.createElement("div");
         d.className="entry";
+        if (i%2 === 0) {
+            d.id = "playerEven";
+        }
         let x=document.createElement("img");
         x.className="icon"+(p.firstPlayer?" important":"");
         x.src="pics/races/" + p.race +""+p.sex+ ".png";
@@ -652,6 +656,7 @@ function populatePlayersAndTeamsList(){
         x.appendChild(s);
         d.appendChild(x);
         list.appendChild(d);
+        i = i + 1;
     });
     d=document.createElement("div");
     d.className="entry";
